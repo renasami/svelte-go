34c246c52de62a3db9b2257e290331ce67b57eac
ren <drrr0502@gmail.com>
2022-04-11 17:20:35 +0900

    忘れてた

--- svelte/src/pages/login.svelte
+++ svelte/src/pages/login.svelte
@@ -1,3 +1,30 @@
-<h1>Login</h1>
-<script type="ts">
-</script>
+<script lang="ts">
+
+    const email = "test@test.co.jp"
+    const password = "test"
+    const method = "POST"
+    const json = {
+        "password": password,
+        "email": email,
+    }
+    const body = JSON.stringify(json)
+    const headers = {
+        "Content-Type": "application/json"
+    
+    }
+    const login = async () => {
+        const response = await fetch("http://localhost:8080/api/login",{headers,method, body})
+        console.log(response.body)
+    
+    }
+
+    const logout = async () => {
+        const response = await fetch("http://localhost:8080/api/logout")
+        console.log(response.body)
+    }
+    </script>
+    <main>
+        <h1>Login</h1>
+        <button on:click={login} >Login</button>
+        <button on:click={logout}>Logout</button>
+    </main>
